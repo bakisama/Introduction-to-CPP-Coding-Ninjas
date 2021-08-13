35cf97c49270e25f529f9aac448ae15b4f02c648
Divyansh Sevta <dsevta@gmail.com>
2021-08-13 22:36:59 +0530

    Update checkcase.cpp

--- Conditionals and Loops/checkcase.cpp
+++ Conditionals and Loops/checkcase.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int c;
+    char x;
+    cin>>x;
+    c=int(x);
+    if(c>=65&&c<=90){
+        cout<<"1";}
+    else if(c>=97&&c<=122){
+        cout<<"0";}
+    else{
+        cout<<"-1";}
+	
+}
 
