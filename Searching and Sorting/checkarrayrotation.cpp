d1cbcd36772b934e931d7be6de0420aac36fc150
Divyansh Sevta <dsevta@gmail.com>
2021-09-10 17:54:54 +0530

    added check array rotation

---
+++ Searching and Sorting/checkarrayrotation.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int arrayRotateCheck(int *arr, int n){
+    // We basically find index of minimum
+    // element
+    int min = arr[0], min_index=0;
+    for (int i=0; i<n; i++)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+
+int main()
+{
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+
+		cout << arrayRotateCheck(input, size) << endl;
+		delete[] input;
+	}
+	
+	return 0;
+}
