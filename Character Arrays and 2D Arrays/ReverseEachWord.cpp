69396a8b77a52269b11fc0cee33dc6d2069d53b9
Divyansh Sevta <dsevta@gmail.com>
2021-09-18 21:53:29 +0530

    updated reverse each word

--- Character Arrays and 2D Arrays/ReverseEachWord.cpp
+++ Character Arrays and 2D Arrays/ReverseEachWord.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
 void reverse(char input[], int i, int j){
     for(; i<=j; i++,j--){
         swap(input[i], input[j]);
@@ -12,4 +15,11 @@ void reverseEachWord(char input[]) {
             j = i + 1;
         }
     }
+}
+int main() {
+    int size = 1e6;
+    char str[size];
+    cin.getline(str, size);
+    reverseEachWord(str);
+    cout << str;
 }
